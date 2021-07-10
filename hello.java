472980a2a4a5a8b487e5e655b044c310c1745036
arjunkotari <arjun.kotari123@gmail.com>
2021-07-10 09:32:59 +0530

    added: DailyWage

--- hello.java
+++ hello.java
@@ -3,6 +3,9 @@ class hello
 	public static void main(String[] a)
 	{
 		System.out.println("Hello");
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+		int dailyWage = 0;
 		int IS_FULL_TIME = 1;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
